86c50abdf4890ea486ce5ec24c51e747402f5338
kiwidata <111706055+kiwidata@users.noreply.github.com>
2022-12-10 22:15:45 -0600

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -14,3 +14,19 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 #P-value = 5.35e-11
 
 #Save your MechaCarChallenge.RScript file to your GitHub repository.
+
+#Download the Suspension_Coil.csv file, and place it in the active directory for your R session.
+
+#In your MechaCarChallenge.RScript, import and read in the Suspension_Coil.csv file as a table.
+Suspension_Coil <- read.csv(file='Suspension_Coil.CSV')
+Suspension_Coil
+#Write an RScript that creates a total_summary dataframe using the summarize() 
+#function to get the mean, median, variance, 
+#and standard deviation of the suspension coil’s PSI column.
+total_summary = Suspension_Coil %>% summarize(Mean_PSI=mean(PSI), median_PSI=median(PSI),variance_PSI = var(PSI), SD_PSI=sd(PSI,na.rm=FALSE))
+total_summary
+#Write an RScript that creates a lot_summary dataframe using the group_by() 
+#and the summarize() functions to group each manufacturing lot by the mean, median, variance, and standard deviation 
+#of the suspension coil’s PSI column.
+lot_summary = Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), median_PSI=median(PSI),variance_PSI = var(PSI), SD_PSI=sd(PSI,na.rm=FALSE))
+lot_summary
